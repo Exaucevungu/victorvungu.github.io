3630abd7d89398079f77827265a0a84c1da39230
Exaucevungu <exaucevungu5@gmail.com>
2026-09-17 16:12:21 +0100

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -92,7 +92,7 @@ menuIcon.addEventListener('click', () => {
 /* ################### SCROLL REVEAL ######################"" */
 
 ScrollReveal({
-	reset: true,
+	reset: false,
 	distance:'80px',
 	duration:2000,
 	delay:200,
